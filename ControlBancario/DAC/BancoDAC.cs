a8614f204fbb8a289220ecc6496a25af91f262dd
jespinozam71181 <julioemilio2003@yahoo.es>
2017-10-30 23:33:07 -0600

    Se agrego la clase de acceso de datos a Banco

---
+++ ControlBancario/DAC/BancoDAC.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Security;
+
+namespace ControlBancario.DAC
+{
+    public static class BancoDAC
+    {
+        public static SqlDataAdapter oAdaptador = InicializarAdaptador();
+
+        private static SqlDataAdapter InicializarAdaptador()
+        {
+            String getSQL = "SELECT  IDBanco ,Codigo ,Descr ,Activo  FROM dbo.cbBanco where IDBanco=@IDBanco";
+            String InsertSQL = "[dbo].[cbUpdateBanco]";
+            String UpdateSQL = "[dbo].[cbUpdateBanco]";
+            String DeleteSQL = "[dbo].[cbUpdateBanco]";
+
+            try
+            {
+                SqlDataAdapter oAdaptador = new SqlDataAdapter()
+                {
+                    SelectCommand = new SqlCommand(getSQL, ConnectionManager.GetConnection()),
+                    InsertCommand = new SqlCommand(InsertSQL, ConnectionManager.GetConnection()),
+                    UpdateCommand = new SqlCommand(UpdateSQL, ConnectionManager.GetConnection()),
+                    DeleteCommand = new SqlCommand(DeleteSQL, ConnectionManager.GetConnection())
+                };
+
+                //Paremetros Select 
+                oAdaptador.SelectCommand.Parameters.Add("@IDBanco", SqlDbType.Int).SourceColumn = "IDBanco";
+                
+
+
+
+                //Paremetros Insert
+                oAdaptador.InsertCommand.CommandType = CommandType.StoredProcedure;
+                oAdaptador.InsertCommand.Parameters.Add("@Operacion", SqlDbType.NChar).Value = "I";
+                oAdaptador.InsertCommand.Parameters.Add("@IDBanco", SqlDbType.Int).SourceColumn = "IDBanco";
+                oAdaptador.InsertCommand.Parameters.Add("@Codigo", SqlDbType.NChar).SourceColumn = "Codigo";
+                oAdaptador.InsertCommand.Parameters.Add("@Descr", SqlDbType.NChar).SourceColumn = "Descr";
+                oAdaptador.InsertCommand.Parameters.Add("@Activo", SqlDbType.Bit).SourceColumn = "Activo";
+
+
+                //Paremetros Update 
+                oAdaptador.UpdateCommand.CommandType = CommandType.StoredProcedure;
+                oAdaptador.UpdateCommand.Parameters.Add("@Operacion", SqlDbType.NChar).Value = "I";
+                oAdaptador.UpdateCommand.Parameters.Add("@IDBanco", SqlDbType.Int).SourceColumn = "IDBanco";
+                oAdaptador.UpdateCommand.Parameters.Add("@Codigo", SqlDbType.NChar).SourceColumn = "Codigo";
+                oAdaptador.UpdateCommand.Parameters.Add("@Descr", SqlDbType.NChar).SourceColumn = "Descr";
+                oAdaptador.UpdateCommand.Parameters.Add("@Activo", SqlDbType.Bit).SourceColumn = "Activo";
+
+
+
+                //Paremetros Delete 
+                oAdaptador.DeleteCommand.CommandType = CommandType.StoredProcedure;
+                oAdaptador.DeleteCommand.Parameters.Add("@Operacion", SqlDbType.NChar).Value = "I";
+                oAdaptador.DeleteCommand.Parameters.Add("@IDBanco", SqlDbType.Int).SourceColumn = "IDBanco";
+                oAdaptador.DeleteCommand.Parameters.Add("@Codigo", SqlDbType.NChar).SourceColumn = "Codigo";
+                oAdaptador.DeleteCommand.Parameters.Add("@Descr", SqlDbType.NChar).SourceColumn = "Descr";
+                oAdaptador.DeleteCommand.Parameters.Add("@Activo", SqlDbType.Bit).SourceColumn = "Activo";
+
+                return oAdaptador;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+        public static void SetTransactionToAdaptador(bool Activo)
+        {
+            oAdaptador.UpdateCommand.Transaction = (Activo) ? ConnectionManager.Tran : null;
+            oAdaptador.DeleteCommand.Transaction = (Activo) ? ConnectionManager.Tran : null;
+            oAdaptador.InsertCommand.Transaction = (Activo) ? ConnectionManager.Tran : null;
+        }
+
+        private static DataSet CreateDataSet()
+        {
+            DataSet DS = new DataSet();
+            DataTable tipoTable = DS.Tables.Add("Data");
+            return DS;
+        }
+
+        public static DataSet GetData( int IDBanco)
+        {
+            DataSet DS = CreateDataSet();
+            oAdaptador.SelectCommand.Parameters["@IDBanco"].Value = IDBanco;
+
+            oAdaptador.Fill(DS.Tables["Data"]);
+            return DS;
+        }
+
+    }
+}
