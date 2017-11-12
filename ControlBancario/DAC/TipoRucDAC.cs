67e1e3d2dc404b5a4a3a0559a25590770536634b
jespinozam71181 <julioemilio2003@yahoo.es>
2017-11-12 05:56:42 -0600

    Se agrego el tipo ruc

---
+++ ControlBancario/DAC/TipoRucDAC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Security;
+
+namespace ControlBancario.DAC
+{
+    public static class TipoRucDAC
+    {
+        public static DataSet GetTipoRuc(int IDTipoRuc)
+        {
+
+            DataSet DS = new DataSet();
+
+            SqlCommand oCmd = new SqlCommand("SELECT IDTipoRuc,Descr  FROM dbo.cbTipoRUC WHERE Activo=1 and (IDTipoRuc=@IDTipoRuc OR @IDTipoRuc =-1)", ConnectionManager.GetConnection());
+            SqlConnection oConn = oCmd.Connection;
+            try
+            {
+
+
+                oCmd.CommandType = CommandType.Text;
+                oCmd.Parameters.Add("@IDTipoRuc", SqlDbType.Int, 50).Value = IDTipoRuc;
+                SqlDataAdapter oAdapatadorTmp = new SqlDataAdapter(oCmd);
+                oAdapatadorTmp.Fill(DS, "Data");
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (oConn.State == ConnectionState.Open)
+                    oConn.Close();
+
+            }
+            return DS;
+
+        }
+    }
+}
